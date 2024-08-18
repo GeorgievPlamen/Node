cd5c0166fc3b46c3ec4b80a991934aec434b4d2a
Plamen Georgiev <plamengeorgiev94dgr@gmail.com>
2024-08-18 13:40:33 +0300

    urlshortener - done

--- urlshortener/index.js
+++ urlshortener/index.js
@@ -1,24 +1,75 @@
-require('dotenv').config();
-const express = require('express');
-const cors = require('cors');
+require("dotenv").config();
+const express = require("express");
+const cors = require("cors");
+const bodyParser = require("body-parser");
+const dns = require("dns");
 const app = express();
 
 // Basic Configuration
 const port = process.env.PORT || 3000;
 
+const postMiddleware = bodyParser.urlencoded({ extended: false });
+
 app.use(cors());
+app.use(postMiddleware);
 
-app.use('/public', express.static(`${process.cwd()}/public`));
+app.use("/public", express.static(`${process.cwd()}/public`));
 
-app.get('/', function(req, res) {
-  res.sendFile(process.cwd() + '/views/index.html');
+app.get("/", function (req, res) {
+  res.sendFile(process.cwd() + "/views/index.html");
 });
 
 // Your first API endpoint
-app.get('/api/hello', function(req, res) {
-  res.json({ greeting: 'hello API' });
+app.get("/api/hello", function (req, res) {
+  res.json({ greeting: "hello API" });
 });
 
-app.listen(port, function() {
+app.get("/api/shorturl/:shortUrl?", (req, res) => {
+  console.log(`shortUrl => ${req.params.shortUrl}`);
+  let originalUrl = urls.get(req.params.shortUrl);
+  res.redirect(originalUrl);
+});
+
+app.post("/api/shorturl", (req, res) => {
+  // let host = getHost(req.body.url);
+  let host = new URL(req.body.url).hostname;
+  dns.lookup(host, (err) => {
+    if (err) {
+      res.json({ error: "invalid url" });
+    } else {
+      let shortUrl = cacheShortUrl(req.body.url);
+      res.json({
+        original_url: req.body.url,
+        short_url: shortUrl,
+      });
+    }
+  });
+});
+
+app.listen(port, function () {
   console.log(`Listening on port ${port}`);
 });
+
+// URL Map
+let urls = new Map();
+let shortUrlIndex = 1000;
+
+function getHost(url) {
+  let firstIndex;
+  let lastIndex;
+
+  for (let i = 0; i < url.length; i++) {
+    if (url[i] === "/" && url[i - 1] === "/") {
+      firstIndex = i + 1;
+    } else if (firstIndex && url[i] === "/") {
+      lastIndex = i;
+    }
+  }
+
+  return url.slice(firstIndex, lastIndex);
+}
+
+function cacheShortUrl(url) {
+  urls.set(`${++shortUrlIndex}`, url);
+  return shortUrlIndex;
+}
